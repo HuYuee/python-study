6f3fccd035baa8bfc6bd4e869da3c7d67c712987
HuYuee <1062887235@qq.com>
2018-05-17 17:22:16 +0800

    update index.py

--- index.py
+++ index.py
@@ -1,2 +1,10 @@
-age = 20
-print(age)
+from datetime import datetime
+from sys import platform
+print(platform)
+odds = [1,3,5,7,9,10]
+right_this_minute = datetime.today().minute
+
+if right_this_minute  in odds:
+    print('This minute ...');
+else :
+    print('NO ..')
