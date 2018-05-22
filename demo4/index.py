baa4b805d06f411f24229fe257e871ab2920878f
HuYuee <1062887235@qq.com>
2018-05-22 16:56:52 +0800

    add the demo of func

---
+++ demo4/index.py
@@ -0,0 +1,13 @@
+# 函数，通过注解来告知用户或者开发者
+
+
+from module import getValue
+
+print(getValue())
+
+
+def xxx(arg: str='xxx'):
+    print(arg)
+
+
+xxx()
---
+++ demo4/module.py
@@ -0,0 +1,7 @@
+# 函数，通过注解来告知用户或者开发者
+
+
+def getValue(word: str='hahha') -> set:
+    """哈哈"""
+    return set(word)
+
